e2c51de4db9505dbe90b9bff0d7a709a019a33e0
officialkushpatel <officialkushpatel@gmail.com>
2020-01-28 20:15:15 +0530

    Update UI

---
+++ layouts/Navbar/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="info" class="nav1">
+      <button class="openbtn" @click="openNav" style="background-color:#9B30FF;">☰</button>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#" class="black-clr">Home</b-nav-item>
+        <b-nav-item href="#" class="black-clr">Quotes</b-nav-item>
+        <b-nav-item href="#" class="black-clr">Admin Panel</b-nav-item>
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <em style="color: black;">John Doe</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      a: false
+    };
+  },
+  methods: {
+    openNav() {
+      if (!this.a) {
+        document.getElementById('main').style.marginLeft = '250px';
+        document.getElementById('mySidebar').style.width = '250px';
+        this.a = !this.a;
+      } else {
+        document.getElementById('mySidebar').style.width = '0';
+        document.getElementById('main').style.marginLeft = '0';
+        this.a = !this.a;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.nav1 {
+  background-color: #d3d3d3 !important;
+}
+.black-clr .nav-link {
+  color: black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,43 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        sample-admin-dashboard
-      </h1>
-      <h2 class="subtitle">
-        My majestic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div id="mySidebar" class="sidebar">
+      <a class="closebtn">Qbook Admin Panel</a>
+      <a href="#" class="text-prpl">
+        <i class="fa fa-home" aria-hidden="true"></i>
+        Dashboard
+      </a>
+    </div>
+
+    <div id="main">
+      <Navbar />
+      <div class="p-3 row nomargin">
+        <Cards name="OEMS" val="12" icon="fa fa-briefcase fa-3x" />
+        <Cards name="PRODUCT TYPES" val="6" icon="fa fa-tag fa-3x" />
+        <Cards name="PRODUCTS" val="742" icon="fa fa-list fa-3x" />
+        <Cards name="ARCHIVED PRODUCTS" val="27" icon="fa fa-recycle fa-3x" />
+      </div>
+      <div class="pb-3 pr-3 pl-3 row nomargin">
+        <BarChart :maxValue="30" class="p-3 col-lg-6 col-md-12 col-sm-12" />
+        <Calender class="p-3 col-lg-6 col-md-6 col-sm-12" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Navbar from '@/layouts/Navbar/index.vue';
+import Cards from '@/components/Dashboard/Cards.vue';
+import BarChart from '@/components/Dashboard/BarChart.vue';
+import Calender from '@/components/Dashboard/Calender.vue';
 
 export default {
   components: {
-    Logo
+    BarChart,
+    Calender,
+    Navbar,
+    Cards
   }
-}
+};
 </script>
 
 <style>
@@ -68,5 +70,80 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+/* here */
+body {
+  font-family: 'Lato', sans-serif;
+}
+
+.sidebar {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #dcdcdc;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidebar a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 18px;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidebar a:hover {
+  color: #9b30ff;
+}
+
+.sidebar .closebtn {
+  position: absolute;
+  top: 0;
+  font-size: 22px;
+}
+
+.openbtn {
+  font-size: 20px;
+  cursor: pointer;
+  background-color: #111;
+  color: white;
+  /* padding: 10px 15px; */
+  border: none;
+}
+
+.openbtn:hover {
+  background-color: #444;
+}
+
+#main {
+  transition: margin-left 0.5s;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidebar {
+    padding-top: 15px;
+  }
+  .sidebar a {
+    font-size: 18px;
+  }
+}
+
+.nomargin {
+  margin-right: 0px !important;
+}
+
+.text-prpl {
+  color: #9b30ff;
+}
+
+.text-prpl a:hover {
+  color: #9b30ff;
 }
 </style>
